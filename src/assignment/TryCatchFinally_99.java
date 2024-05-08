941b3171debbaf154cd2226c54a41c2d27431f4d
Shekhar Palo <paloskrqa@gmail.com>
2024-05-08 18:19:15 +0530

    Update Assignment 99

--- src/assignment/TryCatchFinally_99.java
+++ src/assignment/TryCatchFinally_99.java
@@ -1,10 +1,31 @@
 package assignment;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 //Try , catch and finally keyword usage in one program
 
 public class TryCatchFinally_99 {
 
+	static int age;
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please enter the age: ");
+		
+		try 
+		{
+			age=sc.nextInt();
+		}
+		catch(InputMismatchException e)
+		{
+			System.out.println("Please enter valid data for age!");
+		}
+		finally
+		{
+			System.out.println("This is finally block");
+			sc.close();
+		}
 	}
 
 }
